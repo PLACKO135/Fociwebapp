8a317dafb0b2a6d832379a87b135c95825068147
Placko135 <papplaszlolevente@gmail.com>
2024-09-16 12:56:44 +0200

    Add project files.

---
+++ Fociwebapp/Models/FociDBcontext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fociwebapp.Models
+{
+    public class FociDBcontext : DbContext
+    {
+        public FociDBcontext(DbContextOptions<FociDBcontext> options):base()
+            {
+
+            }
+        public DbSet<Meccs> Meccsek { get; set; }
+    }
+}
+
---
+++ Fociwebapp/Models/Meccs.cs
@@ -0,0 +1,15 @@
+﻿namespace Fociwebapp.Models
+{
+    public class Meccs
+    {
+        public int Id { get; set; }
+        public int Fordulo { get; set; }
+        public int HazaiVeg { get; set; }
+        public int VendegVeg { get; set; }
+        public int HazaiFel { get; set; }
+        public int VendegFel { get; set; }
+        public string? VendegNev { get; set; }
+        public string? HazaiNev { get; set; }
+
+    }
+}
